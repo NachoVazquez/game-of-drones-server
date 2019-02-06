d73fe8552c3cfbe6f04e9986a1f1f75fb1ef15a7
NachoVazquez <nachovc1410@gmail.com>
2019-02-06 15:56:40 -0500

    Creates empty GameOfDronesContext

---
+++ GameOfDrones.DataAccess/Contexts/GameOfDronesContext.cs
@@ -0,0 +1,17 @@
+﻿using GameOfDrones.Core.Abstractions.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameOfDrones.DataAccess.Contexts
+{
+    public class GameOfDronesContext : DbContext, ISqlDbContext
+    {
+        public GameOfDronesContext(DbContextOptions<GameOfDronesContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
