494832ff58f73e12235ff61b68ef5f05865cc9f1
NachoVazquez <nachovc1410@gmail.com>
2019-02-06 15:55:30 -0500

    Creates Abstraction for UnitOfWork pattern

---
+++ GameOfDrones.Core.Abstractions/DataAccess/ISqlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfDrones.Core.Abstractions.DataAccess
+{
+    /// <summary>
+    ///     This interface will be use to resolve the context of the application.
+    /// </summary>
+    public interface ISqlDbContext
+    {
+    }
+}
---
+++ GameOfDrones.Core.Abstractions/DataAccess/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfDrones.Core.Abstractions.DataAccess
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Saves changes to the underlying store asynchronously
+        /// </summary>
+        /// <returns>The number of afected objects</returns>
+        Task<int> SaveChangesAsync();
+
+        /// <summary>
+        /// Executes a raw query to the underlying store asynchronously
+        /// </summary>
+        /// <typeparam name="TResult">The type of the entities returned by the query</typeparam>
+        /// <param name="query">The query to execute</param>
+        /// <param name="queryParams">The query parameters</param>
+        /// <returns>A collection of elements returned by the query</returns>
+        Task<ICollection<TResult>> RawQueryAsync<TResult>(string query, object queryParams = null);
+
+        Task<TResult> QueryFirstAsync<TResult>(string query, object queryParams = null);
+    }
+}
