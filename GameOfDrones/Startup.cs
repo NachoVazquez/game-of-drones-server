f4d64f2fe1fa708cf3df5e567807b3412d14f4f5
NachoVazquez <nachovc1410@gmail.com>
2019-02-07 13:58:03 -0500

    Configure app start up
    * Configure automapper
    * Configure EF
    * Configure Cors
    * Configure Data Layer
    * Configure Business Layer
    * Configure Swagger

--- GameOfDrones/Startup.cs
+++ GameOfDrones/Startup.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.Configuration;
+using GameOfDrones.Business.DependencyRegistry;
+using GameOfDrones.DataAccess.Contexts;
+using GameOfDrones.DataAccess.DependencyRegistry;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace GameOfDrones
 {
@@ -25,6 +41,44 @@ namespace GameOfDrones
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var autoMapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MapperConfigurationExpression());
+            });
+
+            var mapper = autoMapperConfiguration.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+            services.AddDbContext<GameOfDronesContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            #region SET CORS
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllMethodsAndHeaders",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            #endregion
+
+            //adds the configuration of the services for the DataLayer
+            services.AddDataAccessServices();
+
+            //adds the configuration of the services for the BusinessLayer
+            services.AddBusinessServices();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "GameOfDrones Api", Version = "v1"}); });
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,8 +95,22 @@ namespace GameOfDrones
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("AllowAllMethodsAndHeaders");
+
             app.UseMvc();
+
+
+            app.UseSwagger();
+
+            app.UseHttpsRedirection();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GameOfDrones Api");
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            });
         }
     }
 }
