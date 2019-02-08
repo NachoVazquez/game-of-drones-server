3cccbcb92515ce863067f369c38b9a5ee7b89c2c
NachoVazquez <nachovc1410@gmail.com>
2019-02-08 09:27:54 -0500

    Add migrations

---
+++ GameOfDrones.DataAccess/Migrations/20190208141247_AddGameEntities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameOfDrones.DataAccess.Migrations
+{
+    public partial class AddGameEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
