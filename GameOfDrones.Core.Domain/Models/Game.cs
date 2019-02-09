60cb328095cad3c54d0dfdf0b645ed50ba74c9a4
NachoVazquez <nachovc1410@gmail.com>
2019-02-09 11:47:44 -0500

    Add fixes to models

--- GameOfDrones.Core.Domain/Models/Game.cs
+++ GameOfDrones.Core.Domain/Models/Game.cs
@@ -11,8 +11,8 @@ namespace GameOfDrones.Core.Domain.Models
 
         public Player Player1 { get; set; }
         public Player Player2 { get; set; }
-        public int Player1Id { get; set; }
-        public int Player2Id { get; set; }
+        public int? Player1Id { get; set; }
+        public int? Player2Id { get; set; }
 
         public long Player1RoundsWon { get; set; }
         public long Player2RoundsWon { get; set; }
