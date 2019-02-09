1676fd8a8f3ae801e5bbcf4473d94d37c3e2d068
NachoVazquez <nachovc1410@gmail.com>
2019-02-09 11:44:18 -0500

    Create Game viewmodels

---
+++ GameOfDrones/Models/Game/GameCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameOfDrones.Models.Game
+{
+    public class GameCreateViewModel
+    {
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+    }
+}
