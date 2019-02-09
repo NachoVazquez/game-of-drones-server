448133c3ab03d0ccbe29dcbac525f2214993e93a
NachoVazquez <nachovc1410@gmail.com>
2019-02-09 11:46:11 -0500

    Creates AutoMapper Configurations

---
+++ GameOfDrones/Config/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GameOfDrones.Core.Domain.Models;
+using GameOfDrones.Models;
+
+namespace GameOfDrones.Config
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+
+            #region Game configs
+            this.CreateMap<Game, GameIndexViewModel>().AfterMap((src, dest) => {
+                dest.Player1Name = src.Player1.UserName;
+                dest.Player2Name = src.Player2.UserName;
+            }).ReverseMap();
+
+            #endregion
+
+
+
+
+
+        }
+    }
+}
