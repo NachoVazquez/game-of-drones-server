7d12a566742a96c7c8eff10241504cc300aede1c
NachoVazquez <nachovc1410@gmail.com>
2019-02-12 17:24:58 -0500

    Removes outplace logic from UnitOfWork allowing Single Responsability

--- GameOfDrones.DataAccess/UnitOfWork/UnitOfWork.cs
+++ GameOfDrones.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -18,67 +18,34 @@ namespace GameOfDrones.DataAccess.UnitOfWork
 
         public IPlayerRepository PlayerRepository { get; set; }
 
+        public IRoundRepository RoundRepository { get; set; }
 
 
         /// <summary>
         /// Constructor for testing purposes
         /// </summary>
         /// <param name="dbContext"></param>
-        public SqlUnitOfWork(GameOfDronesContext dbContext, IGameRepository gameRepository, IPlayerRepository playerRepository)
+        /// <param name="gameRepository"></param>
+        /// <param name="playerRepository"></param>
+        /// <param name="roundRepository"></param>
+        public SqlUnitOfWork(
+            GameOfDronesContext dbContext,
+            IGameRepository gameRepository,
+            IPlayerRepository playerRepository,
+            IRoundRepository roundRepository)
         {
             DbContext = dbContext;
             GameRepository = gameRepository;
             PlayerRepository = playerRepository;
+            RoundRepository = roundRepository;
         }
 
         public GameOfDronesContext DbContext { get; set; }
-
-        protected string ConnectionStringName { get; set; }
-
-
-        public IDbConnection OpenConnection(out bool closeManually)
-        {
-            var conn = DbContext.Database.GetDbConnection();
-            closeManually = false;
-
-            // Not sure here, should assume always opened??
-            if (conn.State == ConnectionState.Open) return conn;
-
-            conn.Open();
-            closeManually = true;
-
-            return conn;
-        }
 
         public Task<int> SaveChangesAsync()
         {
             return DbContext.SaveChangesAsync();
         }
-
-        public async Task<ICollection<TResult>> RawQueryAsync<TResult>(string query, object queryParams = null)
-        {
-            var connection = OpenConnection(out bool closeConnection);
-            var queryResult = await connection.QueryAsync<TResult>(query, queryParams);
-            if (closeConnection)
-            {
-                connection.Close();
-            }
-
-            return queryResult.ToList();
-        }
-
-        public async Task<TResult> QueryFirstAsync<TResult>(string query, object queryParams = null)
-        {
-            var connection = OpenConnection(out bool closeConnection);
-            var queryResult = await connection.QueryFirstAsync<TResult>(query, queryParams);
-            if (closeConnection)
-            {
-                connection.Close();
-            }
-
-            return queryResult;
-        }
-
 
         public void Dispose()
         {
